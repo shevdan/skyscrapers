caad8a56bf59436c2d62420d09a8665b9da067a7
Bohdan Shevchuk <shevdan@Bohdans-MacBook-Pro.local>
2021-02-04 23:06:09 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="skyscrapers-b.shevchuk",
+    version="0.0.1",
+    author="Bohdan Shevchuk",
+    author_email="shevdan007@gmail.com",
+    description="A module for checking the board for winning situation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=" ",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
