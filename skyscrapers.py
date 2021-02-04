c9f499255ee5194c2045b4ffbde4938cc8f86d38
Bohdan Shevchuk <shevdan@Bohdans-MacBook-Pro.local>
2021-02-04 20:27:10 +0200

    Added files to .gitignore and left_to_right_check to the program

--- skyscrapers.py
+++ skyscrapers.py
@@ -5,8 +5,6 @@ combination on a game board.
 https://github.com/shevdan/skyscrapers
 '''
 
-
-
 def read_input(path: str):
     """
     Read game board file from path.
@@ -23,4 +21,28 @@ def read_input(path: str):
                 continue
             board.append(line)
     return board
+
+        
+def left_to_right_check(input_line: str, pivot: int):
+    """
+    Check row-wise visibility from left to right.
+    Return True if number of building from the left-most hint is visible looking to the right,
+    False otherwise.
+
+    input_line - representing board row.
+    pivot - number on the left-most hint of the input_line.
+`
+    >>> left_to_right_check("412453*", 4)
+    True
+    >>> left_to_right_check("452453*", 5)
+    False
+    """
+    input_line = input_line[1:-1]
+    flag = -1
+    for idx in range(pivot):
+        height = int(input_line[idx])
+        if height <= flag:
+            return False
+        flag = height
+    return True
 
